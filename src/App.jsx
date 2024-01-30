09bfaa6e58c8f522455150b885dcb22c4915b9ca
Saerione <chriss@Saeriones-MacBook-Air.local>
2024-01-29 16:10:40 -0800

    wip_commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import ContactList from './components/ContactList'
+import ContactRow from './components/ContactRow'
 
 function App() {
 
-  return
-  
+  return (
+    <>
+    <ContactList />
+    </>
+  )
 }
 
 export default App
